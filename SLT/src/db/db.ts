62832ef212f2c6d68652b2f70a5cc9b148f0623a
Bruno27866 <bamaral2.git2023@gmail.com>
2025-04-30 18:56:28 -0400

    PUT: applying typescript

--- SLT/src/db/db.ts
+++ SLT/src/db/db.ts
@@ -1,18 +1,11 @@
 /* eslint-disable no-undef */
 
-// 1) Importa todo o pacote 'pg' como um objeto
 import pkg from 'pg';
-
-// 2) Importa o dotenv para carregar o .env
 import dotenv from 'dotenv';
-
-// 3) Carrega as variáveis do .env em process.env
 dotenv.config();
 
-// 4) Puxa o Pool (gestor de conexões) do objeto pkg
 const { Pool } = pkg;
 
-// 5) Lista de variáveis obrigatórias — evita erro de “undefined”
 const requiredEnvVars = [
   'PGHOST',
   'PGPORT',
@@ -26,16 +19,14 @@ requiredEnvVars.forEach((envVar) => {
   }
 });
 
-// 6) Cria o pool de conexões
 const pool = new Pool({
   host:     process.env.PGHOST,
-  port:     Number(process.env.PGPORT),   // CONVERTENDO para number
+  port:     Number(process.env.PGPORT),
   database: process.env.PGDATABASE,
   user:     process.env.PGUSER,
   password: process.env.PGPASSWORD
 });
 
-// 7) Testa a conexão logo ao rodar o app
 pool.connect((err, client, release) => {
   if (err) {
     console.error('Error acquiring client', err.stack);
@@ -45,5 +36,4 @@ pool.connect((err, client, release) => {
   release();
 });
 
-// 8) Exporta o pool para usar em outros módulos
 export default pool;
